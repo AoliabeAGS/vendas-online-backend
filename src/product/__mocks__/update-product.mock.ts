3a114fe8536128db2e6bb3285e9370946b71694e
AoliabeAGS <aoliabe.abadil123@gmail.com>
2024-04-04 10:52:56 -0400

    create unit test in controllers: cart, category, city and product

--- src/product/__mocks__/update-product.mock.ts
+++ src/product/__mocks__/update-product.mock.ts
@@ -1,7 +1,7 @@
-import { categoryMock } from 'src/category/__mocks__/category.mock';
+import { categoryMock } from '../../category/__mocks__/category.mock';
 import { UpdateProductDTO } from '../dtos/update-product.dto';
 
-export const updateProducMock: UpdateProductDTO = {
+export const updateProductMock: UpdateProductDTO = {
   name: 'Product updated',
   image: 'image.png2',
   price: 500,
