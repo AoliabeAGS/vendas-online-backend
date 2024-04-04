023e069e8fba130419abe7bee3f5f76978f9c710
AoliabeAGS <aoliabe.abadil123@gmail.com>
2024-04-04 16:00:45 -0400

    create order controller

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -3,7 +3,7 @@ import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
 import { ProductEntity } from './entities/product.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { CategoryModule } from 'src/category/category.module';
+import { CategoryModule } from '../category/category.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ProductEntity]), CategoryModule],
